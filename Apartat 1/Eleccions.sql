70262841fb247abcb5d5c65233d88462a2beee3e
Santi Onieva <s.onieva@sapalomera.cat>
2023-01-23 20:00:57 +0100

    Sentencia eleccions

--- Apartat 1/Eleccions.sql
+++ Apartat 1/Eleccions.sql
@@ -2,4 +2,4 @@ USE Grup2_eleccions;
 #Volem extreuer el fitxer 01
 
 INSERT INTO eleccions (eleccio_id, nom, data)
-VALUES (1,"eleccions generals 2019", "2019-04-28")
+VALUES (1,"Eleccions Generals 2019", "2019-04-28")
