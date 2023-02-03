209f73c2dd6db28f67ba863b143b9aa89a3eae5d
Santi Onieva <s.onieva@sapalomera.cat>
2023-02-03 18:36:11 +0100

    Programes

--- Apartat 1/importacio_vots_ca.py
+++ Apartat 1/importacio_vots_ca.py
@@ -9,20 +9,22 @@ conexion = mysql.connector.connect(
 )
 cursor=conexion.cursor()
 insert=("INSERT INTO vots_candidatures_ca"
-        "(vots)\n" 
+        "(comunitat_autonoma_id,candidatura_id,vots)\n" 
         "VALUES")
 try :
     with open(path, "r") as fitxer:
         for linia in fitxer:
-            vots=linia[21:28]
-            insert += f'\t({vots}) ,\n'
+            if linia[9:11] != "99":
+                comunitat_autonoma_id = cursor.execute(
+                    f"SELECT comunitat_aut_id FROM comunitats_autonomes WHERE codi_ine = '{linia[9:11]}'")
+                comunitat_autonoma_id = cursor.fetchone()
+                candidatura_id = cursor.execute(
+                    f"SELECT candidatura_id FROM candidatures WHERE codi_candidatura = '{linia[14:20]}'")
+                candidatura_id = cursor.fetchone()
+                vots=int(linia[21:28])
+                insert += f'\t({comunitat_autonoma_id[0]},{candidatura_id[0]},{vots}),\n'
 except OSError as e:
     print("No s'ha pogut obrir el fitxer " + path)
 
-insert=insert.split(" ")
-insert[-1] = ";"
-insert=" ".join(insert)
-cursor.execute(insert)
-conexion.commit()
-cursor.close()
-conexion.close()
+insert = insert[:-2] + ";"
+print(insert)
