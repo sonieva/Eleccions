3b7b96f2b6514ed0481bee0ef4e0cd29ea1e3ed4
Santi Onieva <s.onieva@sapalomera.cat>
2023-02-06 18:37:48 +0100

    a

--- Apartat 1/importacio_eleccions_municipis.py
+++ Apartat 1/importacio_eleccions_municipis.py
@@ -15,11 +15,13 @@ try:
     with open(path, "r", encoding="utf-8") as fitxer:
         for linia in fitxer:
             if linia[16:18] == "99":
-                municipi_id = cursor.execute(f"SELECT municipi_id FROM municipis WHERE (codi_ine = '{linia[13:16]}') AND (provincia_id = (SELECT provincia_id FROM provincies WHERE codi_ine = '{linia[11:13]}'))")
+                municipi_id = cursor.execute(
+                    f"SELECT municipi_id FROM municipis WHERE (codi_ine = '{linia[13:16]}') AND (provincia_id = (SELECT provincia_id FROM provincies WHERE codi_ine = '{linia[11:13]}'))")
                 municipi_id = cursor.fetchone()
                 num_meses = int(linia[136:141])
                 cens = int(linia[157:165]) + int(linia[141:149])
-                vots_emesos = int(linia[189:197]) + int(linia[197:205]) + int(linia[205:213])
+                vots_emesos = int(linia[189:197]) + \
+                    int(linia[197:205]) + int(linia[205:213])
                 vots_valids = int(linia[205:213]) + int(linia[197:205])
                 vots_candidatures = int(linia[205:213])
                 vots_blanc = int(linia[189:197])
