66dc07456b20280359ae4afed4f59fabb7b219a3
Santi Onieva <s.onieva@sapalomera.cat>
2023-02-10 23:12:23 +0100

    Mod. BD_eleccions_v2

--- Apartat 1/BD_eleccions_v2.sql
+++ Apartat 1/BD_eleccions_v2.sql
@@ -7,7 +7,7 @@ ALTER TABLE candidats
 DROP FOREIGN KEY fk_candidats_persones1;
 
 ALTER TABLE persones
-MODIFY COLUMN dni char(9) NULL,
+MODIFY COLUMN dni char(10) NULL,
 MODIFY COLUMN persona_id INT UNSIGNED AUTO_INCREMENT;
 
 ALTER TABLE candidats
@@ -17,3 +17,10 @@ ADD CONSTRAINT fk_candidats_persones1 FOREIGN KEY (persona_id)
 ALTER TABLE municipis
 DROP CONSTRAINT uk_municipis_codi_ine,
 ADD CONSTRAINT uk_municipis_codi_ine_districte_provincia_id UNIQUE (codi_ine,districte,provincia_id);
+
+ALTER TABLE vots_candidatures_mun
+DROP CONSTRAINT fk_candidatures_municipis_eleccions_municipis1,
+ADD CONSTRAINT fk_eleccions_municipis_municipis FOREIGN KEY (municipi_id)
+    REFERENCES municipis(municipi_id),
+ADD CONSTRAINT fk_eleccions_municipis_eleccions FOREIGN KEY (eleccio_id)
+    REFERENCES eleccions(eleccio_id);
