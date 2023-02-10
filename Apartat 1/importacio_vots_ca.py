1ba950dbaffbafc33f4ca5c279f17cf029111b55
Hemant <h.mansukhani@sapalomera.cat>
2023-02-10 16:29:00 +0100

    Programa Llei d'hont

--- Apartat 1/importacio_vots_ca.py
+++ Apartat 1/importacio_vots_ca.py
@@ -1,5 +1,4 @@
 import mysql.connector
-import sys
 path= "Documentació/02201904_MESA/08021904.DAT"
 conexion = mysql.connector.connect(
     host= '10.94.255.166',
@@ -9,13 +8,21 @@ conexion = mysql.connector.connect(
 )
 cursor=conexion.cursor()
 insert=("INSERT INTO vots_candidatures_ca"
-        "(vots)\n" 
+        "(comunitat_autonoma_id,candidatura_id,vots)\n" 
         "VALUES")
 try :
     with open(path, "r") as fitxer:
         for linia in fitxer:
-            vots=linia[21:28]
-            insert += f'\t({vots}) ,\n'
+            if linia[9:11] != "99":
+                candidatura_id= cursor.execute(
+                    f"SELECT candidatura_id FROM candidatures WHERE codi_candidatura = '{linia[14:20]}'"
+                )
+                candidatura_id = cursor.fetchone()
+                comunitat_autonoma_id = cursor.execute(
+                        f"SELECT comunitat_aut_id FROM comunitats_autonomes WHERE codi_ine = '{linia[9:11]}'")
+                comunitat_autonoma_id = cursor.fetchone()
+                vots=int(linia[21:28])
+                insert += f'\t({comunitat_autonoma_id[0]},{candidatura_id[0]},{vots}) ,\n'
 except OSError as e:
     print("No s'ha pogut obrir el fitxer " + path)
 
