d03036b58ac633422241b7917ee4970d3446cbc3
Santi Onieva <s.onieva@sapalomera.cat>
2023-02-10 23:30:00 +0100

    Pujada importacio vots nivell municipal

--- Apartat 1/importacio_vots_mun.py
+++ Apartat 1/importacio_vots_mun.py
@@ -9,26 +9,23 @@ cnx = mysql.connector.connect(
 cursor = cnx.cursor(buffered=True)
 insert = ("INSERT INTO vots_candidatures_mun"
           "(eleccio_id,municipi_id,candidatura_id,vots)\n"
-          "VALUES")
+          "VALUES (%s,%s,%s,%s)")
 try:
     with open(path, "r") as fitxer:
         for linia in fitxer:
-            sql = f"SELECT candidatura_id FROM candidatures WHERE codi_candidatura = '{linia[16:22]}'"
-            candidatura_id = cursor.execute(sql)
-            candidatura_id = cursor.fetchone()
-            municipi_id = cursor.execute(
-                "SELECT municipi_id FROM municipis m "
-                "INNER JOIN provincies p ON p.provincia_id = m.provincia_id " 
-                f"WHERE m.codi_ine = '{linia[11:14]}' " 
-                f"AND p.codi_ine = '{linia[9:11]}'")
-            municipi_id = cursor.fetchone()
-            vots = int(linia[22:30])
-            insert += f'\t(1,{municipi_id[0]},{candidatura_id[0]},{vots}),\n'
+            if linia[14:16] != "99":
+                candidatura_id = cursor.execute(
+                    f"SELECT candidatura_id FROM candidatures WHERE codi_candidatura = '{linia[16:22]}'")
+                candidatura_id = cursor.fetchone()
+                municipi_id = cursor.execute(
+                    f"SELECT municipi_id FROM municipis WHERE (codi_ine = '{linia[11:14]}') AND (districte = '{linia[14:16]}') AND (provincia_id =  (SELECT provincia_id FROM provincies WHERE codi_ine = '{linia[9:11]}'))")
+                municipi_id = cursor.fetchone()
+                vots = int(linia[22:30])
+                cursor.execute(insert,(1,municipi_id[0],candidatura_id[0],vots))
 except OSError as e:
     print("No s'ha pogut obrir el fitxer " + path)
 
 insert = insert[:-2] + ";"
-cursor.execute(insert)
 cnx.commit()
 cursor.close()
 cnx.close()
