83bf546fe3ed54b65d54739f4d501be00f48ab47
Santi Onieva <s.onieva@sapalomera.cat>
2023-02-14 19:49:45 +0100

    Pujada sentencies categoria 3

--- Apartat 2/sentencies_categoria_3.sql
+++ Apartat 2/sentencies_categoria_3.sql
@@ -37,6 +37,5 @@ SELECT p.provincia_id, p.nom, c.nom_curt, c.nom_llarg, v.vots
 	FROM candidatures c
     INNER JOIN vots_candidatures_prov v ON v.candidatura_id = c.candidatura_id
     INNER JOIN provincies p ON p.provincia_id = v.provincia_id
-    WHERE v.vots > (SELECT round(AVG(vots),0)
-						FROM vots_candidatures_prov)
-	ORDER BY v.vots;
+WHERE v.vots > (SELECT ROUND(AVG(vots),0) FROM vots_candidatures_prov)
+ORDER BY v.vots;
