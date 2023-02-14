5a54286b2f9dd0fd6ecdcccde4166e8ac9f5625a
Santi Onieva <s.onieva@sapalomera.cat>
2023-02-14 19:51:25 +0100

    pujada sentencies categoria 3

--- Apartat 2/sentencies_categoria_3.sql
+++ Apartat 2/sentencies_categoria_3.sql
@@ -1,9 +1,7 @@
 -- Mostra el sexe, cognoms i nom de les persones que pertanyin al tipus "T" (Titular)
 SELECT nom, concat(cog1," ",cog2) as cognoms, sexe 
 	FROM persones
-WHERE persona_id IN (	SELECT persona_id 
-							FROM candidats
-						WHERE tipus = "T"	);
+WHERE persona_id IN (SELECT persona_id FROM candidats WHERE tipus = "T");
                         
 -- Mostra el nom de la comunitat autonoma amb el major numero de vots per la candidatura_id = 95
 SELECT nom
@@ -17,9 +15,7 @@ SELECT  m.municipi_id, m.nom, m.codi_ine
 	FROM municipis m
     INNER JOIN provincies p ON p.provincia_id = m.provincia_id
     INNER JOIN comunitats_autonomes c ON c.comunitat_aut_id = p.comunitat_aut_id
-WHERE c.comunitat_aut_id = (	SELECT comunitat_aut_id
-									FROM comunitats_autonomes
-								WHERE upper(nom) = 'CATALUÑA'	)
+WHERE c.comunitat_aut_id = (SELECT comunitat_aut_id FROM comunitats_autonomes WHERE upper(nom) = 'CATALUÑA'	)
 ORDER BY nom;
 
 -- Mostra el nom llarg com nom_partit i el numero de vots de les candidatures votades a nivell provincial de la provincia del municipi de Purchena. Ordena per numero de vots de forma descendent
@@ -27,9 +23,7 @@ SELECT c.nom_llarg AS nom_partit, v.vots
 	FROM vots_candidatures_prov v
     INNER JOIN provincies p ON p.provincia_id = v.provincia_id
     INNER JOIN candidatures c ON c.candidatura_id = v.candidatura_id
-WHERE p.provincia_id = (	SELECT provincia_id 
-								FROM municipis
-							WHERE nom = 'Purchena'	)
+WHERE p.provincia_id = (SELECT provincia_id FROM municipis WHERE nom = 'Purchena'	)
 ORDER BY v.vots DESC;
 
 -- Mostra la provincia_id, el nom de la provincia, el nom curt i el nom llarg de la candidatura i els vots de les candidatures que tinguin un numero de vots superior a les candidatures votades a nivell provincial. Ordena per numero de vots
